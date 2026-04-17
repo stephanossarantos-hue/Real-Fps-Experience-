b94c391b31227e197aaf7182efd99f5ddb773eb3
stephanossarantos-hue <stephanossarantos@gmail.com>
2026-04-17 16:20:09 +0300

    Refactor weapon handling and player movement logic

--- server.js
+++ server.js
@@ -7,66 +7,65 @@ app.use(express.static("."));
 
 let players = {};
 
-const WEAPONS = {
-  ak: { damage: 10, delay: 120 },
-  sniper: { damage: 60, delay: 900 },
-  shotgun: { damage: 25, pellets: 5, spread: 0.2, delay: 700 }
+const weapons = {
+  ak: { damage: 10, spread: 0.02 },
+  sniper: { damage: 60, spread: 0 },
+  shotgun: { damage: 8, pellets: 6, spread: 0.2 }
 };
 
 io.on("connection", (socket) => {
+
   players[socket.id] = {
-    x: 0, y: 1, z: 0,
+    x: 0, y: 2, z: 0,
     hp: 100,
-    skin: 0xff0000,
     weapon: "ak"
   };
 
   socket.emit("init", players);
-
-  socket.broadcast.emit("newPlayer", { id: socket.id, data: players[socket.id] });
+  socket.broadcast.emit("spawn", { id: socket.id, data: players[socket.id] });
 
   socket.on("move", (data) => {
     if (players[socket.id]) {
       players[socket.id].x = data.x;
+      players[socket.id].y = data.y;
       players[socket.id].z = data.z;
+
       socket.broadcast.emit("move", { id: socket.id, data });
     }
   });
 
   socket.on("shoot", (data) => {
-    const shooter = players[socket.id];
+    let shooter = players[socket.id];
     if (!shooter) return;
 
-    const weapon = WEAPONS[shooter.weapon];
+    let w = weapons[shooter.weapon];
 
-    for (let targetId in players) {
-      if (targetId === socket.id) continue;
+    for (let id in players) {
+      if (id === socket.id) continue;
 
-      let t = players[targetId];
+      let p = players[id];
 
-      const dx = t.x - data.x;
-      const dz = t.z - data.z;
-      const dist = Math.sqrt(dx*dx + dz*dz);
+      let dx = p.x - data.x;
+      let dz = p.z - data.z;
+      let dist = Math.sqrt(dx*dx + dz*dz);
 
-      if (dist < 2.5) {
-        t.hp -= weapon.damage;
+      if (dist < 3) {
+        p.hp -= w.damage;
 
-        io.emit("hit", {
-          id: targetId,
-          hp: t.hp
-        });
+        io.emit("hit", { id, hp: p.hp });
 
-        if (t.hp <= 0) {
-          io.emit("death", targetId);
-          t.hp = 100;
-          t.x = 0;
-          t.z = 0;
+        if (p.hp <= 0) {
+          p.hp = 100;
+          p.x = 0;
+          p.z = 0;
+
+          io.emit("death", id);
         }
       }
     }
   });
 
-  socket.on("weaponChange", (w) => {
+  socket.on("weapon", (w) => {
     if (players[socket.id]) {
       players[socket.id].weapon = w;
     }
@@ -78,4 +77,4 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => console.log("Running http://localhost:3000"));
+http.listen(3000, () => console.log("Running"));
