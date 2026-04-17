95059d2b0f5f99bef4550da8cd33af9112fdb78d
stephanossarantos-hue <stephanossarantos@gmail.com>
2026-04-17 16:42:32 +0300

    Refactor player movement and weapon handling

--- server.js
+++ server.js
@@ -7,16 +7,12 @@ app.use(express.static("."));
 
 let players = {};
 
-const weapons = {
-  ak: { damage: 10, spread: 0.02 },
-  sniper: { damage: 60, spread: 0 },
-  shotgun: { damage: 8, pellets: 6, spread: 0.2 }
-};
-
 io.on("connection", (socket) => {
 
   players[socket.id] = {
-    x: 0, y: 2, z: 0,
+    x: 0,
+    y: 0,
+    z: 0,
     hp: 100,
     weapon: "ak"
   };
@@ -25,20 +21,24 @@ io.on("connection", (socket) => {
   socket.broadcast.emit("spawn", { id: socket.id, data: players[socket.id] });
 
   socket.on("move", (data) => {
-    if (players[socket.id]) {
-      players[socket.id].x = data.x;
-      players[socket.id].y = data.y;
-      players[socket.id].z = data.z;
+    if (!players[socket.id]) return;
 
-      socket.broadcast.emit("move", { id: socket.id, data });
+    players[socket.id].x = data.x;
+    players[socket.id].y = data.y;
+    players[socket.id].z = data.z;
+
+    socket.broadcast.emit("move", { id: socket.id, data });
+  });
+
+  socket.on("weapon", (w) => {
+    if (players[socket.id]) {
+      players[socket.id].weapon = w;
     }
   });
 
   socket.on("shoot", (data) => {
     let shooter = players[socket.id];
     if (!shooter) return;
-
-    let w = weapons[shooter.weapon];
 
     for (let id in players) {
       if (id === socket.id) continue;
@@ -47,10 +47,12 @@ io.on("connection", (socket) => {
 
       let dx = p.x - data.x;
       let dz = p.z - data.z;
-      let dist = Math.sqrt(dx*dx + dz*dz);
+
+      let dist = Math.sqrt(dx * dx + dz * dz);
 
       if (dist < 3) {
-        p.hp -= w.damage;
+        p.hp -= shooter.weapon === "sniper" ? 60 :
+                shooter.weapon === "shotgun" ? 25 : 10;
 
         io.emit("hit", { id, hp: p.hp });
 
@@ -65,16 +67,10 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("weapon", (w) => {
-    if (players[socket.id]) {
-      players[socket.id].weapon = w;
-    }
-  });
-
   socket.on("disconnect", () => {
     delete players[socket.id];
     io.emit("remove", socket.id);
   });
 });
 
-http.listen(3000, () => console.log("Running"));
+http.listen(3000, () => console.log("Server running"));
